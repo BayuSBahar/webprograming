6ac11b045ac321a3215a00d1ef12710f421d3a57
BayuSBahar <bayu.bahar.stmik@krw.horizon.ac.id>
2024-06-17 23:35:03 +0700

    my first commit
    first commit

---
+++ oopmvc_crud-main/view/template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/bootstrap/js/bootstrap.min.js" rel="stylesheet">
+    <title><?= $judul ?></title>
+</head>
+
+<body>
+
+    <div class="container"> <?= $isi ?> </div>
+
+</body>
+
+</html>
